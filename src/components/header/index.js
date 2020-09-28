1904048053b246f8f660f5bf775e3555dafad0e7
Barbs <barbarap095@gmail.com>
2020-09-28 17:22:16 +1000

    Add projects

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,52 +10,54 @@ import Mount2 from '../../assets/images/mountain2.png 14-09-49-651.png';
 
 function Heading() {
     return (
-        <>
-            <div className="container-fluid">
-                <Container fluid >
-                    <Col size="sm-8">
-                        <Header as='h2'
-                            className="main-name center aligned header"> Hi there, I'm Barbara Potiriadis</Header>
-                        <Reveal className="resume" animated='move'>
-                            <Reveal.Content visible>
-                                <Image src={Profile} size='small' />
-                            </Reveal.Content>
-                            <Reveal.Content hidden>
-                                <a href={Resume} target="_blank" className="btn btn-primary"
-                                >Resume</a>
-                            </Reveal.Content>
-                        </Reveal>
-                    </Col>
-                    <Grid columns='3'>
+
+        <div className="container-fluid" >
+            <Container fluid >
+                <Col size="sm-8">
+                    <Header as='h2' textAlign='center'
+                        className="main-name center aligned header"> Hi there, <br /> I'm Barbara Potiriadis</Header>
+                    <Reveal className="resume" animated='move'>
+                        <Reveal.Content visible>
+                            <Image src={Profile} size='small' />
+                        </Reveal.Content>
+                        <Reveal.Content hidden>
+                            <a href={Resume} target="_blank" className="btn btn-primary"
+                            >Resume</a>
+                        </Reveal.Content>
+                    </Reveal>
+                </Col>
+                <div className="about">
+                <Grid columns={2}>
+                    <Grid.Column>
                         <div className="about-me-1">
                             Full Stack Web Developer with a background in Market Research bringing a passion for writing clean code
                             to architect creative user-friendly interfaces. Curious and experimental by nature and Willing to leverage my creativity and problem
                             solving skills to build dynamic front and back-end applications.
-                <div />
-                            <div className="about-me-2">
-                                Always attuned to the latest UX/UI designs and thrives on continuous learning and experimentation to develop polished and interactive
-                                applications with a strong attention to detail. I'm an adventurous risk taker, whether I'm in the office or outdoors, who's not
-                                afraid to step outside the boundaries and tackle any challenge with persistance. I have demonstrated a solid
-                                ability to work under pressure in the field of market research to manage an online community/panel, including
-                                survey scripting and designing email marketing campaigns, whilst working on internal projects using statistical
-                                methods to draw inferences and insights for clients.
-                </div>
-                            <div className="about-me-3">
-                                When I'm not coding, you'll find me cooking up a storm in the kitchen, working out and devouring in
-                                ice-cream. Travelling is a core passion of mine, where my travel highlights include the US and Europe. I am
-                                eager to apply the many skills learned in this rewarding course to work as a professional in the field of Web
-                                development.
-                </div>
+                            <br /><br />
+                            Always attuned to the latest UX/UI designs and thrives on continuous learning and experimentation to develop polished and interactive
+                            applications with a strong attention to detail. I'm an adventurous risk taker, whether I'm in the office or outdoors, who's not
+                            afraid to step outside the boundaries and tackle any challenge with persistance. I have demonstrated a solid
+                            ability to work under pressure in the field of market research to manage an online community/panel, including
+                            survey scripting and designing email marketing campaigns, whilst working on internal projects using statistical
+                            methods to draw inferences and insights for clients.
                         </div>
-                    </Grid>
-                    <div className="section">
-                        <img src={Mount1} className="mountain1" alt="mountain-1" />
-                        <img src={Mount2} className="mountain1" alt="mountain-2" />
-                    </div>
-                </Container>
-            </div>
-
-        </>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <div className="about-me-2">
+                            When I'm not coding, you'll find me cooking up a storm in the kitchen, working out and devouring in
+                            ice-cream. Travelling is a core passion of mine, where my travel highlights include the US and Europe. I am
+                            eager to apply the many skills learned in this rewarding course to work as a professional in the field of Web
+                            development.
+                        </div>
+                    </Grid.Column>
+                </Grid>
+                </div>
+                <div className="section">
+                    <img src={Mount1} className="mountain1" alt="mountain-1" />
+                    <img src={Mount2} className="mountain1" alt="mountain-2" />
+                </div>
+            </Container>
+        </div>
     )
 }
 
--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,64 +1,227 @@
 import React from "react";
-import { Button, Icon, Image, Item, Label } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Container, Button, Icon, Image, Item, Label, Grid } from 'semantic-ui-react';
+import { Col } from '../Grid';
+import Burger from '../../assets/images/burger.png';
+import Codequiz from '../../assets/images/codequiz.png';
+import Planner from '../../assets/images/dayplanner.png';
+import Finance from '../../assets/images/finance.png';
+import Project1 from '../../assets/images/project1.png';
+import Notes from '../../assets/images/notetaker.png';
+import Password from '../../assets/images/password.png';
+import Team from '../../assets/images/teamgenerator.png';
+import Employee from '../../assets/images/employeetracker.png';
+import Weather from '../../assets/images/weather2.png';
+import Fitness from '../../assets/images/fitness.png';
+import ReadMe from '../../assets/images/README.png';
 import './style.css';
 
 
 function Project() {
     return (
+        <div className='container'>
+        <Container>
+            <Col size="sm-6">
+            <Grid columns={3} divided>
         <Item.Group divided>
             <Item>
-                <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-
+                <Item.Image src={Project1} />
                 <Item.Content>
-                    <Item.Header as='a'>12 Years a Slave</Item.Header>
-                    <Item.Meta>
-                        <span className='cinema'>Union Square 14</span>
-                    </Item.Meta>
-                    <Item.Description>{paragraph}</Item.Description>
+                    <Item.Header as='a'>My dream Home</Item.Header>
+                    <Item.Description>An application designed to simplify and optimise the house hunting
+                     journey to bring you the most relevant listings.</Item.Description>
                     <Item.Extra>
-                        <Label>IMAX</Label>
-                        <Label icon='globe' content='Additional Languages' />
+                        <Button primary floated='left' 
+                        a href={"https://bdcoelho.github.io/Project1/index.html"} 
+                        target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/bdcoelho/Project1"} 
+                        target="_blank" >Github</Button>   
                     </Item.Extra>
                 </Item.Content>
             </Item>
 
             <Item>
-                <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-
+                <Item.Image src={Team} />
                 <Item.Content>
-                    <Item.Header as='a'>My Neighbor Totoro</Item.Header>
-                    <Item.Meta>
-                        <span className='cinema'>IFC Cinema</span>
-                    </Item.Meta>
-                    <Item.Description>{paragraph}</Item.Description>
+                    <Item.Header as='a'>Team Profile Generator</Item.Header>
+                    <Item.Description>A CLI application that generates a profile of your team!</Item.Description>
                     <Item.Extra>
-                        <Button primary floated='right'>
-                            Buy tickets
-            <Icon name='right chevron' />
-                        </Button>
-                        <Label>Limited</Label>
+                        <Button primary floated='left' 
+                        a href={"https://drive.google.com/file/d/1qb3fogZEOd2BaxS129ipGzi1xCDQ10Ro/view?usp=sharing"} 
+                        target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/Team-Profile-Generator"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+            <Item>
+                <Item.Image src={Burger} />
+                <Item.Content>
+                    <Item.Header as='a'>Eat Da Burger!</Item.Header>
+                    <Item.Description>A Node burger logger application that uses the MVC model to add, devour and remove
+                  burgers!</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://boiling-springs-85381.herokuapp.com/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/Node-Eat-Da-Burger"} 
+                        target="_blank" >Github</Button>   
                     </Item.Extra>
                 </Item.Content>
             </Item>
 
             <Item>
-                <Item.Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-
+                <Item.Image src={Finance} />
                 <Item.Content>
-                    <Item.Header as='a'>Watchmen</Item.Header>
-                    <Item.Meta>
-                        <span className='cinema'>IFC</span>
-                    </Item.Meta>
-                    <Item.Description>{paragraph}</Item.Description>
+                    <Item.Header as='a'>My Stock Pal</Item.Header>
+                    <Item.Description>A finance application that uses MySQL, Express and Node.js to allow users to add,
+                  watch, buy and sell stocks</Item.Description>
                     <Item.Extra>
-                        <Button primary floated='right'>
-                            Buy tickets
-            <Icon name='right chevron' />
-                        </Button>
+                        <Button primary floated='left' 
+                        a href={"https://fast-lake-54569.herokuapp.com/userSummery"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/kube-consult/Project-2-trading-platform"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Fitness} />
+                <Item.Content>
+                    <Item.Header as='a'>Fitness Tracker</Item.Header>
+                    <Item.Description>An application that uses MongoDB to enable 
+                        a user to view, create and keep track of their workouts.</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://nameless-refuge-53137.herokuapp.com/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/NoSQL-Fitness-Tracker"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Employee} />
+                <Item.Content>
+                    <Item.Header as='a'>Employee Tracker</Item.Header>
+                    <Item.Description>Don't lose track of your employees with this Employee Management System that allows
+                  you to add, update and remove employees, roles and departments using MySQL.</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://drive.google.com/file/d/10QKepTv7aqd8F3663eIP48RBgjXbRfRY/view"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/MySQL-Employee-Tracker"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={ReadMe} />
+                <Item.Content>
+                    <Item.Header as='a'>README Generator</Item.Header>
+                    <Item.Description>A CLI application that generates a professional README.md file using node.js</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://drive.google.com/file/d/1BQjPcuoJsPfD1er9SSBebo8AzbEnA-ju/view"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/README-Generator"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Weather} />
+                <Item.Content>
+                    <Item.Header as='a'>Weather Dashboard</Item.Header>
+                    <Item.Description>An application using OpenWeather API that gives you a 5-day detailed weather overview for any city</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://barbara095.github.io/weather-dashboard-HW6/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://barbara095.github.io/weather-dashboard-HW6/"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Codequiz} />
+                <Item.Content>
+                    <Item.Header as='a'>The Ultimate Code Quiz</Item.Header>
+                    <Item.Description>Test your knowledge of full stack web development with this fun quiz.</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://barbara095.github.io/code-quiz-HW4/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/code-quiz-HW4"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Password} />
+                <Item.Content>
+                    <Item.Header as='a'>Password Generator</Item.Header>
+                    <Item.Description>Protect your account and beat the hackers. Create a secure password with this
+                  password generator.</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://barbara095.github.io/password-generator-HW3/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/password-generator-HW3"} 
+                        target="_blank" >Github</Button>   
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+            <Item>
+                <Item.Image src={Planner} />
+                <Item.Content>
+                    <Item.Header as='a'>Work Day Planner</Item.Header>
+                    <Item.Description>Make your work day more organised with this simple work scheduler</Item.Description>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://barbara095.github.io/work-day-scheduler-HW5/"} target="_blank" >App</Button>   
+                    </Item.Extra>
+                    <Item.Extra>
+                        <Button primary floated='left' 
+                        a href={"https://github.com/barbara095/work-day-scheduler-HW5"} 
+                        target="_blank" >Github</Button>   
                     </Item.Extra>
                 </Item.Content>
             </Item>
         </Item.Group>
+        </Grid>
+        </Col>
+    </Container>
+    </div>
     )
 }
 
